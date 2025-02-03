fbfc9e2a5afed3ef0878ec9c0c2af07435305579
ClimDev <miguelangelclimacoflores@gmail.com>
2025-02-02 18:55:35 -0600

    Actualizando experiencia

--- src/data/experiencia.ts
+++ src/data/experiencia.ts
@@ -7,8 +7,13 @@ export type Job = {
 const jobs: Job[] = [
   {
     titulo: "Independiente",
-    periodo: "Agosto - Presente",
+    periodo: "Agosto 2024- Presente",
     descripcion: "Durante dos meses como desarrollador independiente, he diseñado y desarrollado aplicaciones móviles y web utilizando tecnologías modernas como Flutter, Astro y React. Mi experiencia incluye la implementación de interfaces de usuario atractivas y funcionales, así como la integración de APIs. He trabajado en proyectos variados, enfrentando desafíos como la optimización del rendimiento, el diseño responsivo y la adaptación a diferentes plataformas."
+  },
+  {
+    titulo: "PHP Web Developer - SAVE Company",
+    periodo: "Enero 2025 - Presente",
+    descripcion: "Encargado de actualizar paginas web utilizando tecnologias como PHP, Ajax, Jquery."
   },
 ];
 
